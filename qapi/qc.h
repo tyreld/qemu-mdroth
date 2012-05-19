7cb1ef9ab741e79a7894db0a468af3f61d9af502
Michael Roth <mdroth@linux.vnet.ibm.com>
2012-05-18 22:39:37 -0500

    qidl: add misc. visitors

--- qapi/qc.h
+++ qapi/qc.h
@@ -1,9 +1,18 @@
 #ifndef QC_H
 #define QC_H
 
+#include "qemu-timer.h"
+
 #define qc_declaration
 #define _immutable
 #define _derived
 #define _broken
+
+/* misc. visitors */
+void visit_type_tm(Visitor *m, struct tm *obj, const char *name, Error **errp);
+void visit_type_qemu_irq(Visitor *m, void **obj, const char *name,
+                         Error **errp);
+void visit_type_QEMUTimer(Visitor *v, QEMUTimer **obj, const char *name,
+                          Error **errp);
 
 #endif
