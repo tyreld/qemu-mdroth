e9b3572ad87a12d3ebf50c40c68fbd632772c322
Michael Roth <mdroth@linux.vnet.ibm.com>
2012-06-19 14:02:57 -0500

    qidl: codegen, fix license

--- scripts/qidl.py
+++ scripts/qidl.py
@@ -8,7 +8,7 @@
 #  Michael Roth    <mdroth@linux.vnet.ibm.com>
 #
 # This work is licensed under the terms of the GNU GPLv2.
-# See the COPYING.LIB file in the top-level directory.
+# See the COPYING file in the top-level directory.
 
 from ordereddict import OrderedDict
 from qidl_parser import parse_file
