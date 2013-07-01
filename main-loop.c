1f60e45cfc80fd597e52bd1dca0b59c2976aa7ec
Michael Roth <mdroth@linux.vnet.ibm.com>
2013-06-30 22:44:34 -0500

    wait-objects: move handling to a GSource

--- main-loop.c
+++ main-loop.c
@@ -269,54 +269,6 @@ void qemu_del_polling_cb(PollingFunc *func, void *opaque)
     }
 }
 
-/***********************************************************/
-/* Wait objects support */
-typedef struct WaitObjects {
-    int num;
-    int revents[MAXIMUM_WAIT_OBJECTS + 1];
-    HANDLE events[MAXIMUM_WAIT_OBJECTS + 1];
-    WaitObjectFunc *func[MAXIMUM_WAIT_OBJECTS + 1];
-    void *opaque[MAXIMUM_WAIT_OBJECTS + 1];
-} WaitObjects;
-
-static WaitObjects wait_objects = {0};
-
-int qemu_add_wait_object(HANDLE handle, WaitObjectFunc *func, void *opaque)
-{
-    WaitObjects *w = &wait_objects;
-    if (w->num >= MAXIMUM_WAIT_OBJECTS) {
-        return -1;
-    }
-    w->events[w->num] = handle;
-    w->func[w->num] = func;
-    w->opaque[w->num] = opaque;
-    w->revents[w->num] = 0;
-    w->num++;
-    return 0;
-}
-
-void qemu_del_wait_object(HANDLE handle, WaitObjectFunc *func, void *opaque)
-{
-    int i, found;
-    WaitObjects *w = &wait_objects;
-
-    found = 0;
-    for (i = 0; i < w->num; i++) {
-        if (w->events[i] == handle) {
-            found = 1;
-        }
-        if (found) {
-            w->events[i] = w->events[i + 1];
-            w->func[i] = w->func[i + 1];
-            w->opaque[i] = w->opaque[i + 1];
-            w->revents[i] = w->revents[i + 1];
-        }
-    }
-    if (found) {
-        w->num--;
-    }
-}
-
 void qemu_fd_register(int fd)
 {
     WSAEventSelect(fd, event_notifier_get_handle(&qemu_aio_context->notifier),
@@ -378,9 +330,8 @@ static int os_host_main_loop_wait(uint32_t timeout)
     GMainContext *context = g_main_context_default();
     GPollFD poll_fds[1024 * 2]; /* this is probably overkill */
     int select_ret = 0;
-    int g_poll_ret, ret, i, n_poll_fds;
+    int g_poll_ret, ret, n_poll_fds;
     PollingEntry *pe;
-    WaitObjects *w = &wait_objects;
     gint poll_timeout;
     static struct timeval tv0;
     fd_set rfds, wfds, xfds;
@@ -414,28 +365,13 @@ static int os_host_main_loop_wait(uint32_t timeout)
                                       poll_fds, ARRAY_SIZE(poll_fds));
     g_assert(n_poll_fds <= ARRAY_SIZE(poll_fds));
 
-    for (i = 0; i < w->num; i++) {
-        poll_fds[n_poll_fds + i].fd = (DWORD_PTR)w->events[i];
-        poll_fds[n_poll_fds + i].events = G_IO_IN;
-    }
-
     if (poll_timeout < 0 || timeout < poll_timeout) {
         poll_timeout = timeout;
     }
 
     qemu_mutex_unlock_iothread();
-    g_poll_ret = g_poll(poll_fds, n_poll_fds + w->num, poll_timeout);
+    g_poll_ret = g_poll(poll_fds, n_poll_fds, poll_timeout);
     qemu_mutex_lock_iothread();
-    if (g_poll_ret > 0) {
-        for (i = 0; i < w->num; i++) {
-            w->revents[i] = poll_fds[n_poll_fds + i].revents;
-        }
-        for (i = 0; i < w->num; i++) {
-            if (w->revents[i] && w->func[i]) {
-                w->func[i](w->opaque[i]);
-            }
-        }
-    }
 
     if (g_main_context_check(context, max_priority, poll_fds, n_poll_fds)) {
         g_main_context_dispatch(context);
