7b3392bd45ff77d6242694ef871bb6c4001c79d3
Michael Roth <mdroth@linux.vnet.ibm.com>
2013-09-26 17:21:16 -0500

    spapr_pci: fix initial drc node state
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- hw/ppc/spapr.c
+++ hw/ppc/spapr.c
@@ -725,7 +725,7 @@ static void spapr_finalize_fdt(sPAPREnvironment *spapr,
     }
 
     QLIST_FOREACH(phb, &spapr->phbs, list) {
-        drc_entry = spapr_add_phb_to_drc_table(phb->buid, 1 /* Usable */);
+        drc_entry = spapr_add_phb_to_drc_table(phb->buid, 2 /* Unusable */);
         ret = spapr_populate_pci_dt(phb, PHANDLE_XICP, drc_entry->drc_index,
                                     fdt);
     }
